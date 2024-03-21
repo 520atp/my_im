a6509c822b41694901d00c934a449d8be793b24a
xiongliangyu <xiong.liangyu@geelytalent.com.cn>
2024-03-21 18:21:43 +0800

    清理无用入参

--- my-websocket/src/main/java/top/mangod/springwebsocket/constant/SocketConstants.java
+++ my-websocket/src/main/java/top/mangod/springwebsocket/constant/SocketConstants.java
@@ -23,35 +23,4 @@ public interface SocketConstants {
         int BIZ_OPERATE = 3;
     }
 
-    /**
-     * 业务类型
-     */
-    interface BizType {
-        /**
-         *  单聊
-         */
-        int SINGLE_CHAT = 0;
-
-        /**
-         *  群聊
-         */
-        int GROUP_CHAT = 1;
-
-        /**
-         *  系统消息
-         */
-        int SYSTEM_MSG = 2;
-
-    }
-
-    /**
-     * 模块操作类型
-     */
-    interface OperateType {
-        /**
-         * 麦克风操作
-         */
-        int MICRO_PHONE = 1;
-    }
-
 }
